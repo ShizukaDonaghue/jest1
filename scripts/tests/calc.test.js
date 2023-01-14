18264746d59d1afc201c6d2ff92179b276c048ca
Shizuka Donaghue <sdonaghue@gmail.com>
2023-01-14 06:47:55 +0000

    Initial commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,18 @@
+const addition = require('../calc.js');
+
+describe('Calculator', () => {
+    describe('Addition function', () => {
+        Test('should return 42 for 20 + 22', () => {
+            expect(addition(20,22)),toBe(42);
+        })
+    });
+    describe('Subtraction function', () => {
+
+    });
+    describe('Multiply function', () => {
+
+    });
+    describe('Division function', () => {
+
+    });
+})
